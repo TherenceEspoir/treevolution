191055faf7f46bad28da79f206dd4ccca1745d61
houedji.espoir <therencecossi@gmail.com>
2023-10-19 14:39:03 +0200

    feature task1

--- src/treevolution/world.py
+++ src/treevolution/world.py
@@ -1,3 +1,4 @@
+import datetime
 """
 Module which contains World class
 """
@@ -6,3 +7,45 @@ class World():
     """
     World class in order to represent the forest and simulate the evolution of trees
     """
+
+
+    def __init__(self, height, width, start_date):
+        """
+        Constructor of World class
+        """
+        self._height = height
+        self._width = width
+        self._start_date = start_date
+
+
+#methode step qui avance la date d'un jour
+
+    def step(self):
+        """
+        Method qui avance la date d'un jour
+        """
+        self._start_date = self._start_date + datetime.timedelta(days=1)
+
+#methode date qui retourne la date courante
+    
+    @property
+    def date(self):
+        """
+        Method qui retourne la date courante
+        """
+        return self._start_date
+    
+    @property
+    def height(self):
+        """
+        Method qui retourne la hauteur
+        """
+        return self._height
+    
+    @property
+    def width(self):
+        """
+        Method qui retourne la largeur
+        """
+        return self._width
+        
